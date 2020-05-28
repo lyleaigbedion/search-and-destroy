e087574fd0a936ffaf52a149f9272122dea07d10
Nan Pena <peangtan.pena@gmail.com>
2020-05-28 16:12:39 -0400

    binary-search completed

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,6 +2,38 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
+
+
+// edge cases // 
+
+if (array.length === 1 && target === array[0]){
+return true
+}
+if (array.length === 0) return false
+
+
+const midPoint = Math.floor(array.length/2)
+
+if (array[midPoint] === target){
+	return true
+}else {
+
+	if (array[midPoint] > target) {
+		// call binary function and pass newly sliced array //
+		const leftArray = array.slice(0,midPoint)
+		if (binarySearch(leftArray,target)) return true
+		return false
+	}
+	
+	if (array[midPoint] < target) {
+	
+	
+		const rightArray = array.slice(midPoint+1)
+		if (binarySearch(rightArray,target)) return true
+		return false
+	}
+
+}
 	
 };
 
